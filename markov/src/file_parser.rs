488eb9525bea7450faa65a08afd0dabb61daeeea
jesper.bilander <jesper.bilander@miniumtech.com>
2018-03-22 19:04:58 +0100

    string array, each string is a sentence

--- markov/src/file_parser.rs
+++ markov/src/file_parser.rs
@@ -2,11 +2,10 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
-pub fn parse_file(file_name: &str) {
+pub fn parse_file(file_name: &str) -> String {
     let file = File::open(file_name).unwrap();
     let mut buf_reader = BufReader::new(file);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents);
-    println!("{}",contents);
-    assert_eq!(contents, "Hello handsome!\nHello beautiful.\nI am happy to see you.\nI like you.\nYou smell nice.\nYou smell good.\nYou smell very sweet.\nYou are beautiful.");
+    contents
 }
--- markov/src/main.rs
+++ markov/src/main.rs
@@ -2,5 +2,9 @@ mod markovchain;
 use markovchain::MarkovChain;
 mod file_parser;
 fn main() {
-    file_parser::parse_file("markov/resources/nice.txt");
+    let contents = file_parser::parse_file("markov/resources/nice.txt");
+    let splitted = contents.split("\n");
+    let sentences = splitted.collect::<Vec<&str>>();
+    println!("{:?}",sentences);
+
 }
