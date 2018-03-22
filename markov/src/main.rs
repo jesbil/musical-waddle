121996b870defbef973fa1da14bf571fd862a71c
jesper.bilander <jesper.bilander@miniumtech.com>
2018-03-22 19:31:31 +0100

    rude.txt

--- markov/src/main.rs
+++ markov/src/main.rs
@@ -2,9 +2,15 @@ mod markovchain;
 use markovchain::MarkovChain;
 mod file_parser;
 fn main() {
-    let contents = file_parser::parse_file("markov/resources/nice.txt");
+    let contents = file_parser::parse_file("markov/resources/rude.txt");
     let splitted = contents.split("\n");
     let sentences = splitted.collect::<Vec<&str>>();
-    println!("{:?}",sentences);
+    let mut markov = MarkovChain::new();
+
+    for sentence in sentences {
+        markov.parse(sentence);
+    }
+
+    println!("{:?}",markov.generate_sentence());
 
 }
