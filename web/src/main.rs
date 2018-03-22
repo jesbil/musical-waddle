deafc8cceb78f31df759f7238533ebcc7a5977cb
Jonas Hedin <jonas.hedin@cinnober.com>
2018-03-22 16:26:52 +0100

    Changed address bound for web-server to use ip instead of only localhost

--- web/src/main.rs
+++ web/src/main.rs
@@ -3,7 +3,7 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
