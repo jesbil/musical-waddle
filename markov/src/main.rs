bfb432cd44453e423b3253d3499b246798ce8775
jesper.bilander <jesper.bilander@miniumtech.com>
2018-03-23 09:19:36 +0100

    mainchanges

--- markov/src/main.rs
+++ markov/src/main.rs
@@ -2,13 +2,13 @@ mod markovchain;
 use markovchain::MarkovChain;
 mod file_parser;
 fn main() {
-    let contents = file_parser::parse_file("markov/resources/rude.txt");
+    let contents = file_parser::parse_file("markov/resources/nice.txt");
     let splitted = contents.split("\n");
     let sentences = splitted.collect::<Vec<&str>>();
     let mut markov = MarkovChain::new();
 
     for sentence in sentences {
-        markov.add_sentence(sentence);
+        markov.parse(sentence);
     }
 
     println!("{:?}",markov.generate_sentence());
